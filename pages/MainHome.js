3cd93c04f270dd714d18d2e9ae6b2a58900f88cc
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-11 22:08:47 +0600

    home page scroll section touch

--- pages/MainHome.js
+++ pages/MainHome.js
@@ -1,13 +1,14 @@
 import styles from "./carousel.module.css";
 import Image from "next/image";
-import { Button } from "react-bootstrap";
+import { Button, Container, Card } from "react-bootstrap";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import SwiperCore, { Pagination } from "swiper";
+import SwiperCore, { Pagination, Navigation } from "swiper";
 import DatePicking from "./DatePicking";
+import "swiper/css/navigation";
 
-SwiperCore.use([Pagination]);
+SwiperCore.use([Pagination, Navigation]);
 
 const MainHome = () => {
     return (
@@ -173,6 +174,69 @@ const MainHome = () => {
                 </Swiper>
             </div>
             <DatePicking></DatePicking>
+            <Container>
+                <h1
+                    className="text-center"
+                    style={{
+                        marginTop: "100px",
+                        fontFamily: "Cormorant Garamond",
+                        fontSize: "3.5rem",
+                    }}
+                >
+                    {" "}
+                    Urban Spirit of Helsinki
+                </h1>
+                <p className="text-center lead mt-5 mb-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sequi officia eaque perferendis quis dignissimos. Beatae
+                    numquam accusantium iusto nam inventore, a, porro optio nemo
+                    eligendi quia culpa laboriosam reprehenderit dolorum?
+                </p>
+            </Container>
+            <Container>
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    slidesPerGroup={2}
+                    loop={true}
+                    loopFillGroupWithBlank={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    // navigation={true}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <Card style={{ width: "22rem" }}>
+                            <Card.Img
+                                variant="top"
+                                src="https://alloggio.qodeinteractive.com/wp-content/uploads/2020/03/h1-room-img-04.jpg"
+                            />
+                            <Card.Body>
+                                <Card.Title>Modern Home</Card.Title>
+                                <Card.Text>
+                                    <p>from $75 / 75m2 / 1-3 person</p>
+                                </Card.Text>
+                                <Card.Text>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Debitis maiores voluptate
+                                    vero est optio ipsa autem praesentium
+                                    sapiente quisquam a.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide>
+                </Swiper>
+            </Container>
         </>
     );
 };

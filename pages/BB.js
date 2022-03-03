e5ca504f1cb74071d13b6bdcee93caf5460bf9c0
Rafi5734 <kamrulhasun5734@gmail.com>
2022-03-03 17:36:12 +0600

    BB touch

--- pages/BB.js
+++ pages/BB.js
@@ -1,9 +1,49 @@
+import styles from "./BB.module.css";
+import {Carousel, Item} from "react-bootstrap";
 const BB = () => {
-    return (
-        <div>
-            <h1>I am from B&B.</h1>
-        </div>
-    );
+  return (
+    <div className={styles.carouselMain}>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className=""
+            src="https://images.pexels.com/photos/705255/pexels-photo-705255.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className=""
+            src="https://images.pexels.com/photos/1813272/pexels-photo-1813272.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className=""
+            src="https://images.pexels.com/photos/3331488/pexels-photo-3331488.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default BB;
